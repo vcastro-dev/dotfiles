67695a4208151f9cac1f13a42017dadac9fec41e
@vcastro-dev <vcastro2395@gmail.com>
2026-09-14 12:40:59 -0300

    chore: organize project structure

---
+++ scripts/install/config/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+return {
+	default_prog = { 'wsl.exe' },
+	color_scheme = 'Catppuccin Mocha',
+	window_background_opacity = 0.95,
+	window_padding = { left = 12, right = 12, top = 12, bottom = 12 },
+	hide_tab_bar_if_only_one_tab = false,
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+}
